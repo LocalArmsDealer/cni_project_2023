83523289c3ebf05220c88b17916d78cc9afc41e6
LocalArmsDealer <134011323+LocalArmsDealer@users.noreply.github.com>
2023-10-31 15:21:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 // Global Variables
 let cssMode = "default";
-/* const cssOptions = ["default","light","dark","highContrast","none","navyBlue"];
-default is standard
-light mode is bright
-dark mode is dark
-high contrast has high contrast
-none has no css
-navy blue is a darker blue than default*/
+var cssOptions = document.getElementsByType("text/css")[0];
+// Theme Debugging code
+console.log("cssOptions");
