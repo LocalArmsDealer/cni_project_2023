04262e3e51369d0c556852a3534c07e600ecee64
LocalArmsDealer <134011323+LocalArmsDealer@users.noreply.github.com>
2023-10-31 10:26:08 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+// Global Variables
+let cssMode = "default";
+/* const cssOptions = ["default","light","dark","highContrast","none","navyBlue"];
+default is standard
+light mode is bright
+dark mode is dark
+high contrast has high contrast
+none has no css
+navy blue is a darker blue than default*/
